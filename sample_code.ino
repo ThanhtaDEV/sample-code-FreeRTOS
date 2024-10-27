12e5ff7e8d0cf518aa5ab53cc082c2a4ce7baf55
ThanhtaDEV <thanhtadev@gmail.com>
2024-10-27 12:35:37 +0700

    queue

--- sample_code.ino
+++ sample_code.ino
@@ -1,45 +1,72 @@
 #include <Arduino_FreeRTOS.h>
-#include <semphr.h>
+#include <queue.h>
+// mong muốn xây dựng platform cơ bản,
+// có hàm gửi, nhận,có chổ cấu hình queue
+// define massage format
+enum ModuleID
+{
+  //input Rx
+  IN_SW_1801P = ... ,   //... Khai báo cảm biến lên chân digital của Arduino
+  IN_MQ_135 = ... ,     //... Khai báo cảm biến lên chân analog của Arduino
+  IN_PIR = ... ,        //... Khai báo cảm biến lên chân digital của Arduino
+  
+  //output Tx
+  OUT_BUZZER = ... ,    //... khai báo còi vào chân digital trên arduino
+  OUT_FAN = ... ,       //... khai báo quạt vào chân digital trên arduino
+  OUT_LED = ...        //... khai báo led vào chân digital trên arduino
+};
 
-// Đầu vào
-#define SW_1801P ... //... khai báo cảm biến SW_1801P vào chân digital trên arduino
-#define MQ_135 ...  //... khai báo cảm biến MQ_135 vào chân analog on arduino
-#define PIR ...    //... khai báo cảm biến PIR vào chân digital trên arduino
+enum ActionPayload 
+{ 
+  //Buzzer output
+  BUZZER_SW_DISABLE,
+  BUZZER_SW_ENABLE,
 
-// Đầu ra
-#define Buzzer ... //... khai báo còi vào chân digital trên arduino
-#define Quat ...  //... khai báo quạt vào chân digital trên arduino
-#define Led ...  //... khai báo led vào chân degital trên arduino
+  //Fan output
+  FAN_MQ_DISABLE,
+  FAN_MQ_ENABLE,
+  
+  //Led output
+  LED_PIR_DISABLE,
+  LED_PIR_ENABLE
+};
 
-//tạo 3 hàm sermaphore
-semaphore_handle Rung_semaphore;
-semaphore_handle Khoi_semaphore;
-semaphore_handle Person_semaphore;
+struct Message 
+{
+  ModuleID id_Tx;
+  ModuleID id_Rx;
+  ActionPayload payload;
+};
+
+QueueHandle_t RUNG_xQueue;
+QueueHandle_t KHOI_xQueue;
+QueueHandle_t PERSON_xQueue;
 
 void setup() 
 {
   //setup đầu vào
-  pinMode(SW_1801P, INPUT);
-  pinMode(MQ_135, INPUT);
-  pinMode(PIR, INPUT);
-  //setup đầu ra
-  pinMode(Buzzer, OUTPUT);
-  pinMode(Quat, OUTPUT);
-  pinMode(Led, OUTPUT);
+  pinMode(IN_SW_1801P, INPUT);
+  pinMode(IN_MQ_135, INPUT);
+  pinMode(IN_PIR, INPUT);
 
-  Rung_semaphore = xSemaphoreCreateBinary();
-  Khoi_semaphore = xSemaphoreCreateBinary();
-  Person_semaphore = xSemaphoreCreateBinary();
+  //setup đầu ra
+  pinMode(OUT_BUZZER, OUTPUT);
+  pinMode(OUT_FAN, OUTPUT);
+  pinMode(OUT_LED, OUTPUT);
+
+  RUNG_xQueue = xQueueCreate(5, sizeof(Message));
+  KHOI_xQueue = xQueueCreate(5, sizeof(Message));
+  PERSON_xQueue = xQueueCreate(5, sizeof(Message));
 
   // Tạo 3 task thực hiện input
-  xTaskCreate(task_rung, "RUNG", 128, NULL, 1, NULL);
-  xTaskCreate(task_khoi, "KHOI", 128, NULL, 1, NULL);
-  xTaskCreate(task_person, "PERSON", 128, NULL, 1, NULL);
+  xTaskCreate(task_RUNG, "RUNG", 128, NULL, 1, NULL);
+  xTaskCreate(task_KHOI, "KHOI", 128, NULL, 1, NULL);
+  xTaskCreate(task_PERSON, "PERSON", 128, NULL, 1, NULL);
 
   //3 task thực hiện output
-  xTaskCreate(task_rung_output, "RUNG", 128, NULL, 2, NULL);
-  xTaskCreate(task_khoi_output, "KHOI", 128, NULL, 2, NULL);
-  xTaskCreate(task_person_output, "PERSON", 128, NULL, 2, NULL);
+  xTaskCreate(task_RUNG_output, "RUNG", 128, NULL, 2, NULL);
+  xTaskCreate(task_KHOI_output, "KHOI", 128, NULL, 2, NULL);
+  xTaskCreate(task_PERSON_output, "PERSON", 128, NULL, 2, NULL);
 }
 
 void loop() 
@@ -47,78 +74,112 @@ void loop()
   // Không dùng loop khi chạy FreeRTOS
 }
 
-void task_rung(void *pvParameters)
+void task_RUNG(void *pvParameters)
 {
   for(;;)
   {
-    if (digitalRead(SW_1801P) == HIGH)
-    {
-      xSemaphoreGive(Rung_semaphore);
-    }
-void task_rung_output()
+   Message sw_send = {0};
+      sw_send.id_Tx = OUT_BUZZER;
+      sw_send.id_Rx = IN_SW_1801P;
+      sw_send.payload = BUZZER_SW_DISABLE;
+      xQueueSend(RUNG_xQueue, &sw_send, portMAX_DELAY);
+      if(xQueueSend(RUNG_xQueue, &sw_send, portMAX_DELAY) == pdPASS)
+      {
+        // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
+      }
+      else
+      {
+        //Còn không nhận được thì.... ví dụ pir hư thì đóng gói gửi đi
+      }
+  }
+}  
+void task_RUNG_output()
 { 
   for(;;)
   {
-    if(xSemaphoreTask(Rung_semaphore, portMAX_DELAY))
+     Message sw_receive = {0};
+    if(xQueueReceive(RUNG_xQueue, &sw_receive, portMAX_DELAY) == pdTRUE)
+    { 
+      // nhận được từ input đã gửi ở trên thì thực hiện.....
+    }
+    else
     {
-      digitalWrite(Buzzer, HIGH);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-      digitalWrite(Buzzer, LOW)
+      //còn không thì thực hiện......
     }
   }   
 }
 
-void task_khoi(void *pvParameters)
+////////////////////////////
+void task_KHOI(void *pvParameters)
 {
   for(;;)
   {
-    int giatri_MQ = analogRead(MQ_135);
-
-    if(giatri_MQ > ...) //... nồng độ khói mà người dùng muốn cài 
-    {
-      xSemaphoreGive(Khoi_semaphore);
-    }
+    Message mq_send = {0};
+      mq_send.id_Tx = OUT_FAN;
+      mq_send.id_Rx = IN_MQ_135;
+      mq_send.payload = FAN_MQ_DISABLE;
+      xQueueSend(KHOI_xQueue, &mq_send, portMAX_DELAY);
+      if(xQueueSend(KHOI_xQueue, &mq_send, portMAX_DELAY) == pdPASS)
+      {
+        // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
+      }
+      else
+      {
+        //Còn không nhận được thì.... ví dụ pir hư thì đóng gói gửi đi
+      }
   }
 }
-void task_khoi_output(void *pvParameters)
+
+void task_KHOI_output(void *pvParameters)
 {
   for(;;)
   {
-    if(xSemaphoreTask(Khoi_semaphore, portMAX_DELAY))
+    Message mq_receive = {0};
+    if(xQueueReceive(KHOI_xQueue, &mq_receive, portMAX_DELAY) == pdTRUE)
+    { 
+      // nhận được từ input đã gửi ở trên thì thực hiện.....
+    }
+    else
     {
-      digitalWrite(Buzzer, HIGH);
-      digitalWrite(Led, HIGH);
-      digitalWrite(Quat, HIGH);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-      digitalWrite(Buzzer, LOW);
+      //còn không thì thực hiện......
     }
   }  
 }
 
-void task_person(void *pvParameters)
+//////////////////////////////////////
+void task_PERSON(void *pvParameters)
 {
   for(;;)
   {
-    if(digitalRead(PIR) == HIGH);
-    {
-      xSemaphoreGive(Person_semaphore);
-    }
+    Message pir_send = {0};
+      pir_send.id_Tx = OUT_LED;
+      pir_send.id_Rx = IN_PIR;
+      pir_send.payload = LED_PIR_DISABLE;
+      xQueueSend(PERSON_xQueue, &pir_send, portMAX_DELAY);
+      if(xQueueSend(PERSON_xQueue, &pir_send, portMAX_DELAY) == pdPASS)
+      {
+        // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
+      }
+      else
+      {
+        //Còn không nhận được thì.... ví dụ pir hư thì đóng gói gửi đi
+      }
   }
-void task_person_output()
+void task_PERSON_output()
 {
   for(;;)
   {
-    if(xSemaphoreTake(Person_semaphore, portMAX_DELAY))
+    Message pir_receive = {0};
+    if(xQueueReceive(PERSON_xQueue, &pir_receive, portMAX_DELAY) == pdTRUE)
+    { 
+      // nhận được từ input đã gửi ở trên thì thực hiện.....
+    }
+    else
     {
-    digitalWrite(Led, HIGH);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-    digitalWrite(Led, LOW);
+      //còn không thì thực hiện......
     }
   }
 }
-
-//Yều cầu quạt hư thì có cần quản lí thiết bị truyne62 cho con Arduino xử lí rồi
-//in ra màn hình thiết bị hay ko
 
 
 
