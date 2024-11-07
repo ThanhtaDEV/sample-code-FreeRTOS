a933595093386c4dfc64fb596816d6930ceb864a
vixhha <levinhha2k4@gmail.com>
2024-11-07 22:00:49 +0700

    ha_new

--- Project_FreeRTOS/main/task_rung.h
+++ Project_FreeRTOS/main/task_rung.h
@@ -6,55 +6,63 @@
 
 void task_RUNG(void *pvParameters)
 {
-    const int khoi_noiseThreshold = ....; // cập nhật giá trị
-    int khoi_unchangedCount=0;
-    int khoi_lastValue1 = 0;
-    int khoi_lastValue2 = 0;
-    int khoi_count=0;
-    unsigned long khoi_lastSignalTime = 0;
+    const int rung_noiseThreshold = ....; // cập nhật giá trị
+    int rung_unchangedCount=0;
+    int rung_lastValue1 = 0;
+    int rung_lastValue2 = 0;
+    int rung_count=0;
+    unsigned long rung_lastSignalTime = 0;
   for(;;)
   {
-    unsigned long khoi_currentTime = millis(); // đọc giá trị thời gian hiện tại
-    int khoi_value = analogRead(IN_MQ_135);
+    unsigned long rung_currentTime = millis(); // đọc giá trị thời gian hiện tại
 
+
+    int rung_value = analogRead(IN_SW_1801P);
+    Serial.print("Rung Value: ");
+    Serial.println(rung_value); 
    Message sw_send = {0};
       sw_send.id_Tx = OUT_BUZZER;
       sw_send.id_Rx = IN_SW_1801P;
       sw_send.payload = BUZZER_SW_DISABLE;
       xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY);
-      if(xQueueSend(Platform_Queue, &mq_send, portMAX_DELAY) == pdPASS)
+           if(xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY) == pdPASS)
       {
         // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
-        if(abs(khoi_value-khoi_lastValue1)>=khoi_noiseThreshold)
+        if(abs(rung_value-rung_lastValue1)>=rung_noiseThreshold)
         {
-          khoi_count++;
+          rung_count++;
           // ngưỡng báo động
-          if(khoi_value > ... && khoi_count>=... )// ngưỡng giá trị báo động 
+          if(rung_value > ... && rung_count>=... )// ngưỡng giá trị báo động 
           {
-            sw_send.payload = FAN_MQ_ENABLE;
+            sw_send.payload = BUZZER_SW_ENABLE;
             Serial.print("Buzzer on");
-            if(khoi_value>...)
-            // gửi tới loa cảnh báo......z
-            khoi_lastValue1=khoi_value;
+            rung_lastValue1=rung_value;
 
           }
           else 
           {
-          sw_send.payload = FAN_MQ_DISABLE;
+          sw_send.payload = BUZZER_SW_DISABLE;
           Serial.print("Buzzer off");
           }
           
         }
         else
         {
-          khoi_count=0;
-          khoi_lastValue1=khoi_value;
+
+          if(rung_value>....)
+          {
+            sw_send.payload = BUZZER_SW_ENABLE;
+            Serial.print("Buzzer on");
+          }
+          rung_count=0;
+          rung_lastValue1=rung_value;
         }
       }
       else
       {
         //Còn không nhận được thì.... ví dụ pir hư thì đóng gói gửi đi
       }
+      // kiểm tra cảm biến
   }
 }  
 #endif
