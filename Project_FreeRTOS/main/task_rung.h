18b65514994335409fbadf41a3be6c4bbc8b4842
vixhha <levinhha2k4@gmail.com>
2024-11-08 21:57:40 +0700

    task_rung_update

--- Project_FreeRTOS/main/task_rung.h
+++ Project_FreeRTOS/main/task_rung.h
@@ -6,56 +6,50 @@
 
 void task_RUNG(void *pvParameters)
 {
-    const int rung_noiseThreshold = ....; // cập nhật giá trị
-    int rung_unchangedCount=0;
-    int rung_lastValue1 = 0;
-    int rung_lastValue2 = 0;
-    int rung_count=0;
-    unsigned long rung_lastSignalTime = 0;
+    int rung_noiseThreshold = 50; // cập nhật giá trị
+    int rung_lastValue = 0;
+    int rung_count = 0;
   for(;;)
   {
-    unsigned long rung_currentTime = millis(); // đọc giá trị thời gian hiện tại
-
-
-    int rung_value = analogRead(IN_SW_1801P);
+    int rung_currentvalue = analogRead(IN_SW_1801P);
     Serial.print("Rung Value: ");
-    Serial.println(rung_value); 
+    Serial.println(rung_currentvalue); 
    Message sw_send = {0};
       sw_send.id_Tx = OUT_BUZZER;
       sw_send.id_Rx = IN_SW_1801P;
       sw_send.payload = BUZZER_SW_DISABLE;
       xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY);
-           if(xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY) == pdPASS)
+      if(xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY) == pdPASS)
       {
         // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
-        if(abs(rung_value-rung_lastValue1)>=rung_noiseThreshold)
+        if(abs(rung_currentvalue-rung_lastValue)>=rung_noiseThreshold)
         {
           rung_count++;
           // ngưỡng báo động
-          if(rung_value > ... && rung_count>=... )// ngưỡng giá trị báo động 
+          if(rung_currentvalue>=80 && rung_count>=3 )// ngưỡng giá trị báo động 
           {
             sw_send.payload = BUZZER_SW_ENABLE;
             Serial.print("Buzzer on");
-            rung_lastValue1=rung_value;
-
+            rung_lastValue=rung_currentvalue;
+            rung_count=0;
           }
           else 
           {
-          sw_send.payload = BUZZER_SW_DISABLE;
-          Serial.print("Buzzer off");
-          }
-          
-        }
-        else
-        {
-
-          if(rung_value>....)
-          {
-            sw_send.payload = BUZZER_SW_ENABLE;
-            Serial.print("Buzzer on");
-          }
-          rung_count=0;
-          rung_lastValue1=rung_value;
+            rung_count=0;
+            if(rung_currentvalue>=80)
+            {
+              Serial.println("Warning");
+              sw_send.payload = BUZZER_SW_ENABLE;
+              Serial.print("Buzzer on");
+              rung_lastValue=rung_currentvalue;
+            }
+            else
+            {
+              rung_lastValue=rung_currentvalue;
+              sw_send.payload = BUZZER_SW_DISABLE;
+              Serial.print("Buzzer off");
+            }
+          }  
         }
       }
       else
