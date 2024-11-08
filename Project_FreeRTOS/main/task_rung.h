44b1ee163bfe19f9b47efa37b8a8ce3317155cab
vixhha <levinhha2k4@gmail.com>
2024-11-08 22:25:53 +0700

    i swaped and deleted

--- Project_FreeRTOS/main/task_rung.h
+++ Project_FreeRTOS/main/task_rung.h
@@ -15,10 +15,9 @@ void task_RUNG(void *pvParameters)
     Serial.print("Rung Value: ");
     Serial.println(rung_currentvalue); 
    Message sw_send = {0};
-      sw_send.id_Tx = OUT_BUZZER;
-      sw_send.id_Rx = IN_SW_1801P;
+      sw_send.id_Tx = IN_SW_1801P;
+      sw_send.id_Rx = OUT_BUZZER;
       sw_send.payload = BUZZER_SW_DISABLE;
-      xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY);
       if(xQueueSend(Platform_Queue, &sw_send, portMAX_DELAY) == pdPASS)
       {
         // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
