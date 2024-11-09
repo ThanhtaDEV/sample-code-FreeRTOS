c0e452b60064bf5c2fc55b119c5deb643b0104b9
vixhha <levinhha2k4@gmail.com>
2024-11-09 15:57:02 +0700

    [task_rung]update code related to detect disconnected pin

--- Project_FreeRTOS/main/task_rung.h
+++ Project_FreeRTOS/main/task_rung.h
@@ -9,18 +9,49 @@ void task_RUNG(void *pvParameters)
     int rung_noiseThreshold = 50; // cập nhật giá trị
     int rung_lastValue = 0;
     int rung_count = 0;
+
   for(;;)
   {
-    int rung_currentvalue = analogRead(IN_SW_1801P);
+    pinMode(testPin, OUTPUT);
+    digitalWrite(testPin,HIGH);
+    int rung_currentvalue=digitalRead(IN_SW_1801P);
+    //int rung_currentvalue = analogRead(IN_SW_1801P);
     Serial.print("Rung Value: ");
-    Serial.println(rung_currentvalue); 
+    Serial.println(rung_currentvalue);
    Message sw_send = {0};
       sw_send.id_Tx = IN_SW_1801P;
       sw_send.id_Rx = OUT_BUZZER;
       sw_send.payload = BUZZER_SW_DISABLE;
       if(xQueueSend(Rung_Queue, &sw_send, portMAX_DELAY) == pdPASS)
       {
-        // Thực hiện nhiệm vụ của input để đóng gói và gửi đi
+      // kiểm tra cảm biến
+        if(rung_currentvalue==HIGH)
+        {
+          serial.println("pin is disconnected");
+          sw_send.payload=SENSOR_DISCONNECTED;          
+        }
+        else
+        {
+          serial.println("pin is connected");
+          sw_send.payload=SENSOR_CONNECTED;   
+        }
+        digitalWrite(testPin, LOW);
+        rung_currentvalue=digitalRead(IN_SW_1801P);
+        if(rung_currentvalue==LOW)
+        {
+          Serial.println("pin is disconnected");
+          sw_send.payload=SENSOR_DISCONNECTED;  
+        }
+        else
+        {        
+          serial.println("pin is connected");
+          sw_send.payload=SENSOR_CONNECTED;   
+        }
+      pinMode(testPin, INPUT);
+      // kiểm tra chống nhiễu
+        rung_currentvalue = analogRead(IN_SW_1801P);
+        Serial.print("Rung Value: ");
+        Serial.println(rung_currentvalue); 
         if(abs(rung_currentvalue-rung_lastValue)>=rung_noiseThreshold)
         { 
           rung_count++;
